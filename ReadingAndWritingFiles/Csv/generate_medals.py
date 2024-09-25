27ee74be2c4e707d77a58115b30ee9dabcd44c9c
Ayush K Jha <116189541+KAyush16@users.noreply.github.com>
2024-09-25 15:01:30 +0530

    Update generate_medals.py

--- ReadingAndWritingFiles/Csv/generate_medals.py
+++ ReadingAndWritingFiles/Csv/generate_medals.py
@@ -3,7 +3,7 @@ import csv
 ordering = ['Country', 'Gold', 'Silver', 'Bronze', 'Rank']
 # Note the lack of 'Total' in `ordering`
 
-with open('OlympicMedals_2020.csv', encoding='utf-8', newline='') as data
+with open('OlympicMedals_2020.csv', encoding='utf-8', newline='') as data:
     open('medals_dict.py', 'w', encoding='utf-8') as output_file:
     # Write the first part of the code (excluding the actual data)
     print('import csv', file=output_file)
